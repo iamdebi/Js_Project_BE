e4cb417b08d55a91db3541a9770fa9fcce3218af
Debbie <debiurquhart@live.co.uk>
2020-01-31 09:57:22 +0000

    adds to heroku

--- server.js
+++ server.js
@@ -11,22 +11,44 @@ app.use(parser.json());
 
 var url = process.env.MONGOLAB_URI;
 
-MongoClient.connect(url, (error, client) => {
-  if (error) {
-    console.log(error);
-  }
-  const db = client.db("heroku_k04slfgv");
-  const questionCollection = db.collection("questions");
-  const questionsRouter = createRouter(questionCollection);
-  app.use("/api/questions", questionsRouter);
+// MongoClient.connect(url, (error, client) => {
+//   if (error) {
+//     console.log(error);
+//   }
+//   const db = client.db("heroku_k04slfgv");
+//   const questionCollection = db.collection("questions");
+//   const questionsRouter = createRouter(questionCollection);
+//   app.use("/api/questions", questionsRouter);
 
-  const userCollection = db.collection("users");
-  const usersRouter = createRouter(userCollection);
-  app.use("/api/users", usersRouter);
+//   const userCollection = db.collection("users");
+//   const usersRouter = createRouter(userCollection);
+//   app.use("/api/users", usersRouter);
 
-  app.listen(process.env.PORT || 5000, function() {
-    console.log(`app listening on port ${this.address().port}`);
-  });
+//   app.listen(process.env.PORT || 5000, function() {
+//     console.log(`app listening on port ${this.address().port}`);
+//   });
 
-  console.log(db);
+//   console.log(db);
+// });
+
+MongoClient.connect(url)
+  .then(client => {
+    const db = client.db("heroku_k04slfgv");
+    const questionCollection = db.collection("questions");
+    const questionsRouter = createRouter(questionCollection);
+    app.use("/api/questions", questionsRouter);
+  })
+  .catch(console.err);
+
+MongoClient.connect(url)
+  .then(client => {
+    const db = client.db("heroku_k04slfgv");
+    const userCollection = db.collection("users");
+    const usersRouter = createRouter(userCollection);
+    app.use("/api/users", usersRouter);
+  })
+  .catch(console.err);
+
+app.listen(process.env.PORT || 5000, function() {
+  console.log(`app listening on port ${this.address().port}`);
 });
